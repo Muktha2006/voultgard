94137265f1245cb0a622475544902f41d8bda212
Muktha Sree T <mukthasree410@gmail.com>
2026-07-16 10:05:57 -0700

    Improve packet capture details in capture.py
    Enhanced packet capture functionality to display source and destination IPs, as well as TCP/UDP port information.

--- voultgard/PacketInterceptor/capture.py
+++ voultgard/PacketInterceptor/capture.py
@@ -1,10 +1,22 @@
 from scapy.all import sniff
+from scapy.layers.inet import IP, TCP, UDP
 
 def packet_callback(packet):
-    print(packet.summary())
+    if IP in packet:
+        print("=" * 50)
+        print("Source IP      :", packet[IP].src)
+        print("Destination IP :", packet[IP].dst)
+
+        if TCP in packet:
+            print("Protocol       : TCP")
+            print("Source Port    :", packet[TCP].sport)
+            print("Destination Port:", packet[TCP].dport)
+
+        elif UDP in packet:
+            print("Protocol       : UDP")
+            print("Source Port    :", packet[UDP].sport)
+            print("Destination Port:", packet[UDP].dport)
 
 print("Capturing 10 packets...")
-
 sniff(count=10, prn=packet_callback)
-
 print("Capture completed.")
