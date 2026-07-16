9a7648e2bd8acf5835f857d9a7a26c5e2887a788
Muktha Sree T <mukthasree410@gmail.com>
2026-07-16 10:09:16 -0700

    Update parser.py

--- voultgard/PacketInterceptor/parser.py
+++ voultgard/PacketInterceptor/parser.py
@@ -1,15 +1,18 @@
 from scapy.all import sniff
 from scapy.layers.inet import IP, TCP
 
-def parse_modbus(packet):
+def parse_packet(packet):
     if IP in packet and TCP in packet:
-        if packet[TCP].sport == 502 or packet[TCP].dport == 502:
-            print("=" * 50)
-            print("Modbus Packet Detected")
-            print("Source IP      :", packet[IP].src)
-            print("Destination IP :", packet[IP].dst)
-            print("Source Port    :", packet[TCP].sport)
-            print("Destination Port:", packet[TCP].dport)
+        print("=" * 50)
+        print("Source IP      :", packet[IP].src)
+        print("Destination IP :", packet[IP].dst)
+        print("Source Port    :", packet[TCP].sport)
+        print("Destination Port:", packet[TCP].dport)
 
-print("Waiting for Modbus packets on port 502...")
-sniff(prn=parse_modbus)
+        if packet[TCP].sport == 502 or packet[TCP].dport == 502:
+            print(">>> Modbus TCP Packet Detected <<<")
+
+print("Listening for packets...")
+sniff(count=10, prn=parse_packet)
+
+print("Finished.")
